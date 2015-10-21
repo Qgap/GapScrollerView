283c785ca5fc6b4820cb445bcbce8798e8c7c47c
Gao Qing <gaoqingqing@meihuishuo.com>
2015-10-21 18:46:18 +0800

    Initial Commit

---
+++ GapScrollerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GapScrollerView
+//
+//  Created by Mac on 15/10/21.
+//  Copyright © 2015年 Gap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
